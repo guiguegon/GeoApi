e95ec552ca4053f85f5d495c1d3f89c13e9d3d0e
DESKTOP-J190QGB\Guille <guiguegon@gmail.com>
2016-11-12 12:44:12 +0100

    Added BaseContract and BasePresenter

---
+++ app/src/main/java/es/guiguegon/geoapi/components/ui/BaseContract.java
@@ -0,0 +1,14 @@
+package es.guiguegon.geoapi.components.ui;
+
+/**
+ * Created by Guille on 12/11/2016.
+ */
+
+public interface BaseContract {
+
+    interface View {
+    }
+
+    interface ActionListener {
+    }
+}
