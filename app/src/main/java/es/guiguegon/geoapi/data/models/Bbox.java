1bc7d39b17050c7050fb76d8a0823b745fda87c6
DESKTOP-J190QGB\Guille <guiguegon@gmail.com>
2016-11-13 11:38:19 +0100

    Parcelables models

--- app/src/main/java/es/guiguegon/geoapi/data/models/Bbox.java
+++ app/src/main/java/es/guiguegon/geoapi/data/models/Bbox.java
@@ -1,15 +1,37 @@
 package es.guiguegon.geoapi.data.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Guille on 12/11/2016.
  */
 
-public class Bbox {
+public class Bbox implements Parcelable {
 
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Bbox> CREATOR = new Parcelable.Creator<Bbox>() {
+        @Override
+        public Bbox createFromParcel(Parcel in) {
+            return new Bbox(in);
+        }
+
+        @Override
+        public Bbox[] newArray(int size) {
+            return new Bbox[size];
+        }
+    };
     private double south;
     private double east;
     private double north;
     private double west;
+
+    protected Bbox(Parcel in) {
+        south = in.readDouble();
+        east = in.readDouble();
+        north = in.readDouble();
+        west = in.readDouble();
+    }
 
     public double getSouth() {
         return south;
@@ -25,5 +47,18 @@ public class Bbox {
 
     public double getWest() {
         return west;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(south);
+        dest.writeDouble(east);
+        dest.writeDouble(north);
+        dest.writeDouble(west);
     }
 }
--- app/src/main/java/es/guiguegon/geoapi/data/models/Location.java
+++ app/src/main/java/es/guiguegon/geoapi/data/models/Location.java
@@ -1,15 +1,37 @@
 package es.guiguegon.geoapi.data.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by guiguegon on 12/11/2016.
  */
 
-public class Location {
+public class Location implements Parcelable {
 
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Location> CREATOR = new Parcelable.Creator<Location>() {
+        @Override
+        public Location createFromParcel(Parcel in) {
+            return new Location(in);
+        }
+
+        @Override
+        public Location[] newArray(int size) {
+            return new Location[size];
+        }
+    };
     private String name;
     private Bbox bbox;
     private String lat;
     private String lng;
+
+    protected Location(Parcel in) {
+        name = in.readString();
+        bbox = (Bbox) in.readValue(Bbox.class.getClassLoader());
+        lat = in.readString();
+        lng = in.readString();
+    }
 
     public String getName() {
         return name;
@@ -26,5 +48,17 @@ public class Location {
     public String getLng() {
         return lng;
     }
-}
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeValue(bbox);
+        dest.writeString(lat);
+        dest.writeString(lng);
+    }
+}
